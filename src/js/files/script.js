f2aea74e99e2cb4e262a5c76a667b206c7d8eb96
Adam Gastiev <agast6335@gmail.com>
2023-04-24 15:06:40 +0300

    qwerty

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -8,6 +8,12 @@ ymaps.ready(function () {
 		center: [55.76, 37.64],
 		zoom: 7,
 	});
+	myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+	myMap.controls.remove('searchControl'); // удаляем поиск
+	myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+	myMap.controls.remove('typeSelector'); // удаляем тип
+	myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+	myMap.controls.remove('rulerControl'); // удаляем контрол правил
 	myMap.behaviors.disable('scrollZoom');
 	const centralPlacemark = new ymaps.Placemark(
 		[55.76, 37.64],
